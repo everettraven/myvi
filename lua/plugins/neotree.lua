a377ab8fff65fb178dbb9f4515f9551f838aae50
everettraven <everettraven@gmail.com>
2024-04-16 15:46:09 -0400

    update configs
    Signed-off-by: everettraven <everettraven@gmail.com>

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+          never_show = {
+            ".git",
+            ".DS_Store",
+          },
+        },
+      },
+      window = {
+        position = "float",
+      },
+    },
+  },
+}
